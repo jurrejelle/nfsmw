fc4225fedd4cd7383bf3a5e3b360e6bcf2a29fcb
jurrejelle <jurre@jilles.com>
2026-09-09 00:07:01 +0200

    zWorld: UpdateTires computes is_flat before onground

--- src/Speed/Indep/Src/World/CarRenderConn.cpp
+++ src/Speed/Indep/Src/World/CarRenderConn.cpp
@@ -875,8 +875,10 @@ void CarRenderConn::UpdateTires(float dT, float carspeed, const RenderConn::Pkt_
 
     for (unsigned int i = 0; i < 4; i++) {
         unsigned int axle = i >> 1;
-        bool onground = ((data.mGroundState >> i) & 1U) != 0;
+        bool onground;
         bool is_flat = ((data.mBlowOuts >> i) & 1U) != 0;
+
+        onground = ((data.mGroundState >> i) & 1U) != 0;
         TireState *state = this->mTireState[i];
 
         eIdentity(&this->mTireMatrices[i]);
